7d386462ccbae493b97cfed1d4080dc08399a446
Jaroslaw Kedziorek <jarevock@gmail.com>
2018-01-06 10:57:23 +0100

    initial commit

---
+++ src/main/java/pl/kedziorek/silka/silownia/SilowniaApplication.java
@@ -0,0 +1,12 @@
+package pl.kedziorek.silka.silownia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SilowniaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SilowniaApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/kedziorek/silka/silownia/SilowniaApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.kedziorek.silka.silownia;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SilowniaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
